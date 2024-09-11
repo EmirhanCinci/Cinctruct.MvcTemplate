5e8ec17c439b8da570ae5220d9e359709854c46d
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-11 14:00:31 +0300

    Added SessionExtensions with methods for setting and getting objects in session.

---
+++ Cintruct/Cintruct.MvcUi/Extensions/SessionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Cintruct.MvcUi.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static void SetObject(this ISession session, string key, object data)
+        {
+            session.SetString(key, JsonSerializer.Serialize(data));
+        }
+
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var sessionJsonData = session.GetString(key);
+            if (sessionJsonData != null)
+            {
+                return JsonSerializer.Deserialize<T>(sessionJsonData);
+            }
+            return default(T);
+        }
+    }
+}
