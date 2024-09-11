5ebe72132a0b0384ef3a87798dc74e6cfdafe29c
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-11 14:05:48 +0300

    Added CacheControlHandler middleware to disable caching in HTTP responses.

---
+++ Cintruct/Cintruct.MvcUi/Middlewares/CacheControlHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Cintruct.MvcUi.Middlewares
+{
+    public class CacheControlHandler
+    {
+        private readonly RequestDelegate _next;
+        public CacheControlHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+            context.Response.Headers["Expires"] = "-1";
+            await _next(context);
+        }
+    }
+}
