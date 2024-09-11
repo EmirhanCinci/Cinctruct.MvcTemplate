bbb07e3d0bb6eadc67d0c3b0345381b8a9744522
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-11 13:56:07 +0300

    Added IHttpApiService with CRUD methods.

---
+++ Cintruct/Cintruct.MvcUi/ApiServices/Implementations/HttpApiService.cs
@@ -0,0 +1,100 @@
+﻿using Cintruct.MvcUi.ApiServices.Interfaces;
+using Microsoft.Net.Http.Headers;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
+
+namespace Cintruct.MvcUi.ApiServices.Implementations
+{
+    public class HttpApiService : IHttpApiService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public HttpApiService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T> DeleteDataAsync<T>(string service, string endPoint, string? token = null)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri($"{_configuration[$"ServiceUrl:{service}"]}{endPoint}"),
+                Headers = { { HeaderNames.Accept, "application/json" } }
+            };
+            if (!string.IsNullOrEmpty(token))
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            var responseMessage = await client.SendAsync(requestMessage);
+            var jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+            var response = JsonSerializer.Deserialize<T>(jsonResponse, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            return response;
+        }
+
+        public async Task<T> GetDataAsync<T>(string service, string endPoint, string? token = null)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{_configuration[$"ServiceUrl:{service}"]}{endPoint}"),
+                Headers = { { HeaderNames.Accept, "application/json" } }
+            };
+            if (!string.IsNullOrEmpty(token))
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            var responseMessage = await client.SendAsync(requestMessage);
+            var jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+            var response = JsonSerializer.Deserialize<T>(jsonResponse, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            return response;
+        }
+
+        public async Task<T> PostDataAsync<T>(string service, string endPoint, string? jsonData = null, string? token = null)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri($"{_configuration[$"ServiceUrl:{service}"]}{endPoint}"),
+                Headers = { { HeaderNames.Accept, "application/json" } },
+            };
+            if (!string.IsNullOrEmpty(jsonData))
+            {
+                requestMessage.Content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            }
+            if (!string.IsNullOrEmpty(token))
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            var responseMessage = await client.SendAsync(requestMessage);
+            var jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+            var response = JsonSerializer.Deserialize<T>(jsonResponse, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            return response;
+        }
+
+        public async Task<T> PutDataAsync<T>(string service, string endPoint, string jsonData, string? token = null)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"{_configuration[$"ServiceUrl:{service}"]}{endPoint}"),
+                Headers = { { HeaderNames.Accept, "application/json" } },
+                Content = new StringContent(jsonData, Encoding.UTF8, "application/json")
+            };
+            if (!string.IsNullOrEmpty(token))
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            var responseMessage = await client.SendAsync(requestMessage);
+            var jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+            var response = JsonSerializer.Deserialize<T>(jsonResponse, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            return response;
+        }
+    }
+}
---
+++ Cintruct/Cintruct.MvcUi/ApiServices/Interfaces/IHttpApiService.cs
@@ -0,0 +1,10 @@
+﻿namespace Cintruct.MvcUi.ApiServices.Interfaces
+{
+    public interface IHttpApiService
+    {
+        Task<T> DeleteDataAsync<T>(string service, string endPoint, string? token = null);
+        Task<T> GetDataAsync<T>(string service, string endPoint, string? token = null);
+        Task<T> PostDataAsync<T>(string service, string endPoint, string? jsonData = null, string? token = null);
+        Task<T> PutDataAsync<T>(string service, string endPoint, string jsonData, string? token = null);
+    }
+}
